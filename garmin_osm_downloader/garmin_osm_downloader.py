3b7d7eb503189a3707f1504b2a85f964eae53b4c
Marian Hahne <marian@marianhahne.de>
2024-10-12 14:59:21 +0200

    Refactor garmin_osm_downloader.py to download OSM files for Garmin etrex 20x and create a Todoist task for successful downloads

--- garmin_osm_downloader/garmin_osm_downloader.py
+++ garmin_osm_downloader/garmin_osm_downloader.py
@@ -2,11 +2,14 @@ import os
 import sys
 import requests
 from datetime import datetime
+from todoist_api_python.api import TodoistAPI
 
 def download_files(urls, root_folder):
     timestamp = datetime.now().strftime('%Y%m%d')
     download_folder = os.path.join(root_folder, timestamp)
     os.makedirs(download_folder, exist_ok=True)
+
+    all_downloaded = True
 
     for url in urls:
         filename = os.path.join(download_folder, os.path.basename(url))
@@ -22,11 +25,27 @@ def download_files(urls, root_folder):
             print(f"Downloaded {url} to {filename}")
         except requests.exceptions.RequestException as e:
             print(f"Failed to download {url}: {e}")
+            all_downloaded = False
+
+    if all_downloaded:
+        create_todoist_task("Neue Maps auf Garmin kopieren", "Neue Karten liegen unter: " + download_folder)
 
 def load_urls_from_file(file_path):
     with open(file_path, 'r') as file:
         urls = [line.strip() for line in file if line.strip()]
     return urls
+
+def create_todoist_task(task_content, task_description):
+    api_token = os.getenv('TODOIST_API_TOKEN')
+    api = TodoistAPI(api_token)
+    task = api.add_task(content=task_content, due_string="today", description=task_description)
+    print(task)
+
+    if not api_token:
+        print("Todoist API token not found in environment variables.")
+        return
+
+    
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
